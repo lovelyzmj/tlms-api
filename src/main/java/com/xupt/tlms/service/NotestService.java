1aae001247bdd143747211e77800dd7ee13454e8
lovelyzmj <Bigzhi_1028@163.com>
2019-04-12 21:02:51 +0800

    notes

--- src/main/java/com/xupt/tlms/service/NotestService.java
+++ src/main/java/com/xupt/tlms/service/NotestService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public interface NotestService {
     boolean writeNotes(Notes notes);
-    List<Notes> getSelfNotes(int userId);//查看自己的所有笔记
+    List<NotesVo> getSelfNotes(int userId);//查看自己的所有笔记
     List<NotesVo> getNotesByliteratureId(int literatureId);//根据文献id查看该文件下所有笔记
     boolean updateNotes(Notes notes);//根据id 修改笔记内容或者状态
     boolean deleteNotes(int id);//删除笔记
--- src/main/java/com/xupt/tlms/vojo/NotesVo.java
+++ src/main/java/com/xupt/tlms/vojo/NotesVo.java
@@ -5,8 +5,11 @@ import java.io.Serializable;
 public class NotesVo implements Serializable {
     private int id;//笔记id
     private String content;//笔记内容
+    private int userId;//作者id
     private String nickName;//笔记作者昵称
     private long createTime;//发表时间
+    private int literatureId;//所属文献Id
+    private String literatureName;//所属文献名称
     private int status;//笔记状态 （0：公开  1：私有）
 
     public int getId() {
@@ -49,13 +52,40 @@ public class NotesVo implements Serializable {
         this.status = status;
     }
 
+    public String getLiteratureName() {
+        return literatureName;
+    }
+
+    public void setLiteratureName(String literatureName) {
+        this.literatureName = literatureName;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getLiteratureId() {
+        return literatureId;
+    }
+
+    public void setLiteratureId(int literatureId) {
+        this.literatureId = literatureId;
+    }
+
     @Override
     public String toString() {
         return "NotesVo{" +
                 "id=" + id +
                 ", content='" + content + '\'' +
+                ", userId=" + userId +
                 ", nickName='" + nickName + '\'' +
                 ", createTime=" + createTime +
+                ", literatureId=" + literatureId +
+                ", literatureName='" + literatureName + '\'' +
                 ", status=" + status +
                 '}';
     }
